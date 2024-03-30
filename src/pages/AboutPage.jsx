640c7c1b8eefe62c6b9f5840be007094ab4a55db
Pilar1936 <tesoro4me@hotmail.com>
2024-03-30 13:53:30 -0500

    Modify style and add images

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,65 +1,57 @@
 // This is a static page mocking an "About Us" section for our fake user data
+import React from 'react';
+import '../components/styles/about.css'; 
+import { Fade, Roll } from 'react-reveal';
+import mariaImage from '../assets/perfil.jpg'; 
 export default function AboutPage() {
   return (
     <div className="container pt-4">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
-      </p>
-      <section className="features-icons bg-light text-center m-4">
-        <div className="container">
-          <div className="row p-2">
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-window m-auto text-primary" />
-                </div>
-                <h3>Fully Responsive</h3>
-                <p className="lead mb-0">
-                  This theme will look great on any device, no matter the size!
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-layers m-auto text-primary" />
-                </div>
-                <h3>Bootstrap 5 Ready</h3>
-                <p className="lead mb-0">
-                  Featuring the latest build of the new Bootstrap 5 framework!
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-                <div className="features-icons-icon d-flex">
-                  <i className="bi-terminal m-auto text-primary" />
-                </div>
-                <h3>Easy to Use</h3>
-                <p className="lead mb-0">
-                  Ready to use with your own content, or customize the source
-                  files!
-                </p>
-              </div>
-            </div>
-          </div>
+      <div className="about-container">
+        <div className="about-text">
+          <Fade>
+            <h2>Maria Jones</h2>
+          </Fade>
+          <Roll right>
+            <p className="text-left">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+              tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
+              impedit suscipit sed magnam alias in, repellat expedita hic explicabo
+              architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Velit voluptate exercitationem quaerat pariatur
+              mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
+              nostrum temporibus ad omnis nam rerum eligendi.
+            </p>
+            <p className="text-left">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+              tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
+              impedit suscipit sed magnam alias in, repellat expedita hic explicabo
+              architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Velit voluptate exercitationem quaerat pariatur
+              mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
+              nostrum temporibus ad omnis nam rerum eligendi.
+            </p>
+          </Roll>
         </div>
-      </section>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-        impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-        architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-        mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-        nostrum temporibus ad omnis nam rerum eligendi.
-      </p>
+        <Roll bottom>
+          <div className="about-image">
+            <img src={mariaImage} alt="Maria" /> {/* Si aún necesitas la imagen de María */}
+          </div>
+        </Roll>
+      </div>
+
+      {/* Contenedor para las imágenes a centrar */}
+      <div className="image-container">
+        <img src={reactIcon} alt="React Icon" />
+        <img src={javastIcon} alt="javas Icon" />
+        <img src={htmltIcon} alt="html Icon" />
+        <img src={sqltIcon} alt="sql Icon"/>
+      </div>
+      
+     {/* Footer */}
+     <footer className="footer bg-dark fixed-bottom d-flex justify-content-center align-items-center">
+        <a href="https://github.com/Pilar1936" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+      </footer>
     </div>
   );
 }
