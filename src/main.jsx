ac0eb24b19e980abfaf8ec42934c30e6c9b2c35f
Pilar1936 <tesoro4me@hotmail.com>
2024-03-30 14:28:26 -0500

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,27 @@
-import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+// Importa los componentes de las páginas
 // Bringing in the pages the router will use to conditionally show the appropriate views
+
 import App from './App';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
 import AboutPage from './pages/AboutPage';
+import ProjectPage from './pages/ProjectPage'; 
+import ContactPage from './pages/ContactForm'; 
 
+
+// Define la configuración de las rutas
 // Define the accessible routes, and which components respond to which URL
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -23,19 +32,24 @@ const router = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
-      {
-        path: 'profile/:id',
-        element: <ProfilePage />,
-      },
+      
       {
         path: 'about',
         element: <AboutPage />,
       },
+      {
+        path: 'projects',
+        element: <ProjectPage />,
+      },
+      {
+        path: 'Contact',
+        element: <ContactPage/>,
+      }
     ],
   },
 ]);
 
-// Render the RouterProvider component
+// Renderiza el componente RouterProvider
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
